47b82b930cbad5535c6200d3263b312567d23635
thoppe <travis.hoppe@gmail.com>
2016-03-07 00:37:55 -0500

    Updated polysem

--- polysem.py
+++ polysem.py
@@ -1,38 +1,62 @@
-from ksvd import KSVD
-import h5py, os
-from gensim.models.word2vec import Word2Vec
+import numpy as np
+from sklearn.cluster import AffinityPropagation as cluster_clf
+from scipy.spatial.distance import cdist
+from scipy.cluster.hierarchy import dendrogram, linkage
 
+max_t = 250
+
+target = "Skull"
+#target = "Smiling Face With Open Mouth and Smiling Eyes"
+target = "EMOJI_"+target.lower().replace(" ","_")
+                 
 # Load the config files
 from configobj import ConfigObj
-wcon = ConfigObj("config.ini")["word2vec"]
-kcon = ConfigObj("config.ini")["kSVD"]
+config = ConfigObj("config.ini")
+wcon = config["word2vec"]   
 
 from gensim.models.word2vec import Word2Vec
 f_features = wcon["f_features"].format(**wcon)
 clf = Word2Vec.load(f_features)
-X = clf.syn0
 
-print clf
-print X.shape
 
-result = KSVD(X,
-              dict_size=kcon.as_int("basis_size"),
-              target_sparsity=kcon.as_int("sparsity"),
-              max_iterations=kcon.as_int("iterations"),
-              enable_printing=True,
-              enable_threading = True,
-              print_interval=1)
+def tweet_iterator(limit=None):
+    counter = 0
+    with open("all_unique_tweets.txt") as FIN:
+        for line in FIN:
+            if target in line:
+                yield line.split()
+                counter += 1
+                if counter == limit: break
 
-D,gamma = result
 
-f_model = kcon["f_kSVD"].format(**kcon)
+V = []
+for tokens in tweet_iterator(max_t):
+    v = np.array([clf[t] for t in tokens if t in clf])
+    V.append( v.mean(axis=0) )
 
-h5 = h5py.File(f_model,'w')
-h5.create_dataset("D",data=D, compression="gzip")
-h5.create_dataset("gamma",data=gamma, compression="gzip")
+V = np.array(V)
+DV = cdist(V,V,metric='cosine')
+print DV
 
-# Save the arguments (maybe later?)
-#for key in args:
-#    g.attrs[key] = cargs[key]
+import seaborn as sns
+plt = sns.plt
 
-h5.close()
+# Values are sometimes "slightly" less than zero due to rounding
+DV[DV<0] = 0
+
+cluster_args = {"damping":0.9}
+cluster = cluster_clf(**cluster_args)
+
+y_labels = cluster.fit_predict(DV)
+idx = np.argsort(y_labels)
+
+affinity_DV = DV[idx,:][:,idx]
+y_labels = y_labels[idx]
+    
+#sns.heatmap(DV,xticklabels=False,yticklabels=False,linewidths=0)
+#plt.figure()
+sns.heatmap(affinity_DV,xticklabels=False,yticklabels=False,linewidths=0)
+plt.show()
+
+
+
