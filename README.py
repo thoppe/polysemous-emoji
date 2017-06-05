5035834c49603d022653c9bf31fbc68233126e7c
Travis Hoppe <travis.hoppe@gmail.com>
2017-06-05 16:59:01 -0400

    Adjust links in README

---
+++ README.py
@@ -0,0 +1,6 @@
+### Presentation
+
+http://thoppe.github.io/polysemous-emoji/HnT_pres.html#/
+
+First presented at [DC Hack && Tell Round 30: Didacts and Narpets](https://www.meetup.com/DC-Hack-and-Tell/events/227724648/).
+
